38fa4b458c63a7ed0789a1e0815c4f48cdec825b
pasindupramod755 <pasidubandara12345@gmail.com>
2025-12-01 19:17:28 +0530

    Add Task Complete

--- src/main/java/contrallor/appContrallor.java
+++ src/main/java/contrallor/appContrallor.java
@@ -1,15 +1,19 @@
 package contrallor;
 
+import dto.TaskDTO;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import service.AppService;
 
 public class appContrallor {
+
+    AppService taskService = new AppService();
+    ObservableList<TaskDTO> taskDTOObservableList = taskService.getAllComplete();
+    int x = taskService.getNextIndex();
+    int gridIndex = 1;
 
     @FXML
     private TableColumn<?, ?> colDate;
@@ -43,7 +47,26 @@ public class appContrallor {
 
     @FXML
     void btnAddTask(ActionEvent event) {
+        TaskDTO taskDTO = new TaskDTO(
+                1,
+                txtTitle.getText(),
+                String.valueOf(txtDate.getValue()),
+                txtDescription.getText()
+        );
 
+        taskService.addTask(taskDTO);
+        System.out.println(taskDTO);
+        lblTask.setText("");
+        CheckBox checkBox = new CheckBox(txtTitle.getText());
+        checkBox.setStyle(
+                "-fx-font-size: 20px;" +
+                        "-fx-padding: 5;" +
+                        "-fx-background-color: transparent;"
+        );
+        checkBox.setId(x+"");
+        gridTask.add(checkBox,0,gridIndex);
+        gridIndex++;
+        x++;
     }
 
     @FXML
@@ -53,7 +76,7 @@ public class appContrallor {
 
     @FXML
     void btnDelete(ActionEvent event) {
-        
+
     }
 
 }
---
+++ src/main/java/dbConnection/DBConnection.java
@@ -0,0 +1,26 @@
+package dbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+        private static DBConnection instance;
+        private Connection connection;
+
+        public DBConnection() throws SQLException {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todo","root","1234");
+        }
+
+        public static DBConnection getInstance() throws SQLException {
+            if (instance == null){
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+        public Connection getConnection(){
+            return connection;
+        }
+
+}
